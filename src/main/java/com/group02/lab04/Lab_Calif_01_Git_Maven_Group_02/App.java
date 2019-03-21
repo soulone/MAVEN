0441439e9d8683bcaf5313e7083e9b0976f54bc8
soulone <ascf24032@gmail.com>
2019-03-21 12:21:48 -0500

    Sebastian commit-01 (Subida)(Paquete "view",Clase "ViewUsers")

---
+++ src/main/java/com/group02/lab04/Lab_Calif_01_Git_Maven_Group_02/App.java
@@ -0,0 +1,13 @@
+package com.group02.lab04.Lab_Calif_01_Git_Maven_Group_02;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
